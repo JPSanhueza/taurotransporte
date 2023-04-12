6fe7f972f670aed9ea7f4f55901f174d807fce84
Juan Pablo Sanhueza <jpablosanhueza19@gmail.com>
2023-04-12 14:13:35 -0400

    cambios en los vehiculos

--- resources/views/components/vehiculos.blade.php
+++ resources/views/components/vehiculos.blade.php
@@ -2,29 +2,47 @@
     <h2 class="mb-10 text-5xl text-center ">{{__('Vehículos')}}</h2>
       <div class="grid grid-cols-1 gap-3 mx-auto sm:grid-cols-3 max-w-7xl sm:px-6 lg:px-8">
            
-            <div class="max-w-sm mx-auto mt-10 rounded overflow-hidden shadow-lg">
-                <img class="w-full h-56 object-cover" src="{{asset('img/vehiculos/20170908035113-TRANSIT-MINIBÚS-2017.webp')}}" alt="Imagen de ejemplo">
+            <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
+                <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/ford-transit.png')}}" alt="Imagen de ejemplo">
                 <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Ford Transit</div>
-                        <p class="text-gray-700 text-base">
+                    <div class="mb-2 text-xl font-bold">Ford Transit</div>
+                        <p class="text-base text-gray-700">
                         Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
                         </p>
                 </div>
             </div>
-            <div class="max-w-sm mx-auto mt-10 rounded overflow-hidden shadow-lg">
-                <img class="w-full h-56 object-cover" src="{{asset('img/vehiculos/kiaFrontier.jpg')}}" alt="Imagen de ejemplo">
+            <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
+                <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/KiaFrontierCarga.png')}}" alt="Imagen de ejemplo">
                 <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Kia Frontier</div>
-                        <p class="text-gray-700 text-base">
+                    <div class="mb-2 text-xl font-bold">Kia Frontier</div>
+                        <p class="text-base text-gray-700">
                         Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
                         </p>
                 </div>
             </div>
-            <div class="max-w-sm mx-auto mt-10 rounded overflow-hidden shadow-lg">
-                <img class="w-full h-56 object-cover" src="{{asset('img/vehiculos/citroenJumper2.webp')}}" alt="Imagen de ejemplo">
+            <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
+                <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/citroenJumper2.webp')}}" alt="Imagen de ejemplo">
                 <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Citroen Jumper</div>
-                        <p class="text-gray-700 text-base">
+                    <div class="mb-2 text-xl font-bold">Citroen Jumper</div>
+                        <p class="text-base text-gray-700">
+                        Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
+                        </p>
+                </div>
+            </div>
+            <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
+                <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/changan.png')}}" alt="Imagen de ejemplo">
+                <div class="px-6 py-4">
+                    <div class="mb-2 text-xl font-bold">Changan</div>
+                        <p class="text-base text-gray-700">
+                        Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
+                        </p>
+                </div>
+            </div>
+            <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
+                <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/renault.png')}}" alt="Imagen de ejemplo">
+                <div class="px-6 py-4">
+                    <div class="mb-2 text-xl font-bold">Renault</div>
+                        <p class="text-base text-gray-700">
                         Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
                         </p>
                 </div>

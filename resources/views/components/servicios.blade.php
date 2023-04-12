13f6297a1b68ea8f80fd84af67c0bb8adbcfab9f
Juan Pablo Sanhueza <jpablosanhueza19@gmail.com>
2023-04-12 16:20:48 -0400

    se agrega texto en vehiculos y servicios

--- resources/views/components/servicios.blade.php
+++ resources/views/components/servicios.blade.php
@@ -4,12 +4,23 @@
       <div class="bg-white rounded-sm shadow-xl p-7">
         <img height="300" width="300" class="mx-auto rounded-full" src="{{asset('img/servicios/servicios1.jpg')}}" alt="Desarrollo Web">
         <h3 class="py-3 text-xl font-semibold text-center">{{__('Arriendo de Vehículos de carga')}}</h3>
-        <p>{{__('Texto descriptivo del servicio')}}.</p>
+        <p>Solución flexible y rentable para el transporte de mercancías <br/>
+          Amplia gama de camiones y furgones adaptadas a tus necesidades <br/>
+          Tarifas por días, semanas o meses <br/>
+          Ahorra en costos de compra, mantenimiento y depreciación <br/>
+          Vehículos de distintas capacidades y características <br/>
+          Optimiza tus operaciones logísticas y garantiza la seguridad de tus mercancías
+</p>
       </div>
       <div class="p-7 bg-white rounded-[3px] shadow-xl">
         <img height="300" width="300" class="mx-auto rounded-full" src="{{asset('img/servicios/servicios2.jpg')}}" alt="Maquetacion Web">
         <h3 class="py-3 text-xl font-semibold text-center">{{__('Transporte de carga')}}</h3>
-        <p>{{__('Texto descriptivo del servicio')}}.</p>
+        <p>Expertos en el transporte seguro y eficiente de tus mercancías <br/>
+          Flota de vehículos de carga de diversas capacidades y características <br/>
+          Servicio personalizado y adaptado a tus necesidades logísticas <br/>
+          Tarifas competitivas y transparentes, ajustadas a tus requerimientos <br/>
+          Profesionales capacitados para manejar y cuidar tu carga <br/>
+          Entregas puntuales y confiables para mantener tu negocio en movimiento</p>
       </div>       
     </div>          
 </section>
--- resources/views/components/vehiculos.blade.php
+++ resources/views/components/vehiculos.blade.php
@@ -7,7 +7,7 @@
                 <div class="px-6 py-4">
                     <div class="mb-2 text-xl font-bold">Ford Transit</div>
                         <p class="text-base text-gray-700">
-                        Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
+                            Capacidad de carga: 1200kg.
                         </p>
                 </div>
             </div>
@@ -16,7 +16,7 @@
                 <div class="px-6 py-4">
                     <div class="mb-2 text-xl font-bold">Kia Frontier</div>
                         <p class="text-base text-gray-700">
-                        Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
+                         Capacidad de carga: 1200kg.
                         </p>
                 </div>
             </div>
@@ -25,25 +25,25 @@
                 <div class="px-6 py-4">
                     <div class="mb-2 text-xl font-bold">Citroen Jumper</div>
                         <p class="text-base text-gray-700">
-                        Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
+                        Capacidad de carga: 1200kg.
                         </p>
                 </div>
             </div>
             <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
                 <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/changan.png')}}" alt="Imagen de ejemplo">
                 <div class="px-6 py-4">
-                    <div class="mb-2 text-xl font-bold">Changan</div>
+                    <div class="mb-2 text-xl font-bold">Changan M201</div>
                         <p class="text-base text-gray-700">
-                        Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
+                        Capacidad de carga: 750kg.
                         </p>
                 </div>
             </div>
             <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
                 <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/renault.png')}}" alt="Imagen de ejemplo">
                 <div class="px-6 py-4">
-                    <div class="mb-2 text-xl font-bold">Renault</div>
+                    <div class="mb-2 text-xl font-bold">Renault Dokker</div>
                         <p class="text-base text-gray-700">
-                        Aquí va el texto descriptivo de la tarjeta. Puedes agregar más información sobre el contenido de la tarjeta aquí.
+                        Capacidad de carga: 750kg.
                         </p>
                 </div>
             </div>

446580131833bbbccb40e612e09ab7adae48cfa2
Juan Pablo Sanhueza <jpablosanhueza19@gmail.com>
2023-04-19 14:30:34 -0400

    se agrega nuevo servicio arriendo de vehiculos normales"

--- resources/views/components/servicios.blade.php
+++ resources/views/components/servicios.blade.php
@@ -1,26 +1,23 @@
 <section class="py-20 text-gray-700 bg-gray-100" id="servicios">
     <h2 class="mb-12 text-4xl text-center sm:text-5xl">{{__('Nuestros Servicios')}}</h2>
-    <div class="grid grid-cols-1 gap-6 mx-3 md:grid-cols-2 lg:grid-cols-2 sm:mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="grid grid-cols-1 gap-6 mx-3 md:grid-cols-2 lg:grid-cols-3 sm:mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div class="bg-white rounded-sm shadow-xl p-7">
+        <img height="300" width="300" class="mx-auto rounded-full" src="{{asset('img/servicios/vehiculoParticularArriendo.jpg')}}" alt="Desarrollo Web">
+        <h3 class="py-3 text-xl font-semibold text-center">{{__('Arriendo de Vehículos')}}</h3>
+        <p>Disfruta de un paseo con estilo, arrendando un vehículo particular para salir con amigos, pareja o familia y crea recuerdos inolvidables.
+        </p>
+      </div>
       <div class="bg-white rounded-sm shadow-xl p-7">
         <img height="300" width="300" class="mx-auto rounded-full" src="{{asset('img/servicios/servicios1.jpg')}}" alt="Desarrollo Web">
         <h3 class="py-3 text-xl font-semibold text-center">{{__('Arriendo de Vehículos de carga')}}</h3>
-        <p>Solución flexible y rentable para el transporte de mercancías <br/>
-          Amplia gama de camiones y furgones adaptadas a tus necesidades <br/>
-          Tarifas por días, semanas o meses <br/>
-          Ahorra en costos de compra, mantenimiento y depreciación <br/>
-          Vehículos de distintas capacidades y características <br/>
-          Optimiza tus operaciones logísticas y garantiza la seguridad de tus mercancías
-</p>
+        <p>
+          Transporte flexible y rentable con variedad de vehículos adaptados a tus necesidades, tarifas flexibles y seguridad garantizada.
+        </p>
       </div>
-      <div class="p-7 bg-white rounded-[3px] shadow-xl">
+      <div class="bg-white rounded-sm shadow-xl p-7">
         <img height="300" width="300" class="mx-auto rounded-full" src="{{asset('img/servicios/servicios2.jpg')}}" alt="Maquetacion Web">
         <h3 class="py-3 text-xl font-semibold text-center">{{__('Transporte de carga')}}</h3>
-        <p>Expertos en el transporte seguro y eficiente de tus mercancías <br/>
-          Flota de vehículos de carga de diversas capacidades y características <br/>
-          Servicio personalizado y adaptado a tus necesidades logísticas <br/>
-          Tarifas competitivas y transparentes, ajustadas a tus requerimientos <br/>
-          Profesionales capacitados para manejar y cuidar tu carga <br/>
-          Entregas puntuales y confiables para mantener tu negocio en movimiento</p>
+        <p>Transporte seguro y eficiente con flota variada, servicio personalizado, tarifas competitivas y entregas confiables.
       </div>       
     </div>          
 </section>

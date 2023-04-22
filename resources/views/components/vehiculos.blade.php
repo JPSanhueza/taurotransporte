f3c01600ca98af3ae5b02a04bbcc475af5fa5d33
Juan Pablo Sanhueza <jpablosanhueza19@gmail.com>
2023-04-22 14:14:36 -0400

    se agregan 2 vehiculos

--- resources/views/components/vehiculos.blade.php
+++ resources/views/components/vehiculos.blade.php
@@ -47,6 +47,24 @@
                         </p>
                 </div>
             </div>
+            <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
+                <img class="object-cover w-full h-52" src="{{asset('img/vehiculos/nissan.png')}}" alt="Imagen de ejemplo">
+                <div class="px-6 py-8">
+                    <div class="mb-2 text-xl font-bold">Nissan Qashqai</div>
+                        <p class="text-base text-gray-700">
+                        Manual - Rendimiento mixto hasta 17.8 km/L .
+                        </p>
+                </div>
+            </div>
+            <div class="max-w-sm mx-auto mt-10 overflow-hidden rounded shadow-lg">
+                <img class="object-cover w-full h-56" src="{{asset('img/vehiculos/mini.png')}}" alt="Imagen de ejemplo">
+                <div class="px-6 py-4">
+                    <div class="mb-2 text-xl font-bold">Mini 3 puertas</div>
+                        <p class="text-base text-gray-700">
+                        Manual - Rendimiento mixto hasta 12 km/L.
+                        </p>
+                </div>
+            </div>
            
       </div>          
 </section>

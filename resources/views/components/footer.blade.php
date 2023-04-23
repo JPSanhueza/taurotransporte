dff792ef9868aa04bbe81e81da560a4f39a482e5
Juan Pablo Sanhueza <jpablosanhueza19@gmail.com>
2023-04-23 16:43:48 -0400

    se elimina del footer enlace a twitter y linkedin

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -11,29 +11,20 @@
           <p class="mail-contacto">contacto@atiye.cl</p>
         </div> --}}
 
-        <div class="flex items-center justify-center">
-          <a href="https://www.facebook.com/profile.php?id=100068686771241" class="text-gray-100 no-underline" aria-label="Siguenos en las redes sociales">
+        <div class="flex items-center justify-center pt-4 sm:pt-0">
+          <a href="" class="text-gray-100 no-underline" aria-label="Siguenos en las redes sociales">
               <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a href="https://twitter.com/ATIYE_SpA" class="ml-3 text-gray-100 no-underline" aria-label="Siguenos en las redes sociales">
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a href="https://www.instagram.com/atiye_desarrollo_web/" class="ml-3 text-gray-100 no-underline" aria-label="Siguenos en las redes sociales">
+          
+            <a href="" class="ml-3 text-gray-100 no-underline" aria-label="Siguenos en las redes sociales">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a href="https://www.linkedin.com/company/atiye-desarrollo-web" class="ml-3 text-gray-100 no-underline" aria-label="Siguenos en las redes sociales">
-              <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
-                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-              </svg>
-            </a>
+           
         </div>
     </div>
     <div class="text-center mt-3 text-[9px] sm:text-xs text-gray-100">

bb34008936e53628bb346a9e34b9e539f9e266fa
Juan Pablo Sanhueza <jpablosanhueza19@gmail.com>
2023-04-27 11:15:20 -0400

    se solucionan varios bugs

--- resources/views/components/servicios.blade.php
+++ resources/views/components/servicios.blade.php
@@ -1,5 +1,5 @@
 <section class="py-20 text-gray-700 bg-gray-100" id="servicios">
-    <h2 class="mb-12 text-4xl text-center sm:text-5xl">{{__('Nuestros Servicios')}}</h2>
+    <h2 class="mb-12 text-center text-5xl">{{__('Nuestros Servicios')}}</h2>
     <div class="grid grid-cols-1 gap-6 mx-3 md:grid-cols-2 lg:grid-cols-3 sm:mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="bg-white rounded-sm shadow-xl p-7">
         <img height="300" width="300" class="mx-auto rounded-full" src="{{asset('img/servicios/vehiculoParticularArriendo.jpg')}}" alt="Desarrollo Web">
@@ -18,6 +18,6 @@
         <img height="300" width="300" class="mx-auto rounded-full" src="{{asset('img/servicios/servicios2.jpg')}}" alt="Maquetacion Web">
         <h3 class="py-3 text-xl font-semibold text-center">{{__('Transporte de carga')}}</h3>
         <p>Transporte seguro y eficiente con flota variada, servicio personalizado, tarifas competitivas y entregas confiables.
-      </div>       
-    </div>          
+      </div>
+    </div>
 </section>
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -4,15 +4,15 @@
 <div class="p-6 bg-gray-100 shadow-xl">
     <form wire:submit.prevent="submit">
         @csrf
-        <label class="text-xs text-gray-700 uppercase dark:text-gray-100" for="name">{{__('Nombre')}}</label>
+        <label class="text-xs text-gray-700 uppercase " for="name">{{__('Nombre')}}</label>
         <span class="text-xs text-red-600 "> @error ('nombre') {{$message}}  @enderror</span>
         <input wire:model="nombre" type="text" id="name" name="nombre" class="w-full mb-4 border-gray-200 rounded" value="{{old('nombre')}}">
 
-        <label class="text-xs text-gray-700 uppercase dark:text-gray-100" for="email">Email</label>
+        <label class="text-xs text-gray-700 uppercase " for="email">Email</label>
         <span class="text-xs text-red-600 "> @error ('email') {{$message}}  @enderror</span>
         <input wire:model="email" type="text" id="email" name="email" class="w-full mb-4 border-gray-200 rounded" value="{{old('email')}}">
 
-        <label class="text-xs text-gray-700 uppercase dark:text-gray-100" for="msg">{{__('Mensaje')}}</label>
+        <label class="text-xs text-gray-700 uppercase " for="msg">{{__('Mensaje')}}</label>
         <span class="text-xs text-red-600 "> @error ('msg') {{$message}}  @enderror</span>
         <textarea wire:model="msg" id="msg" name="msg" class="w-full mb-4 border-gray-200 rounded" id="" cols="30" rows="5"></textarea>
         <div class="text-center">
